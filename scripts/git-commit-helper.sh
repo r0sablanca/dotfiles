9a8d10e7f375c4b83454629a347e6d2025c6981e
RosaBlanca <rosablanca@mailfence.com>
2023-07-14 16:57:15 -0700

    :art: Riced: git-commit-help.sh

--- scripts/git-commit-helper.sh
+++ scripts/git-commit-helper.sh
@@ -4,9 +4,9 @@
 #
 # July 14, 2023
 TYPE=$(gum choose ":rocket: Initial Commit" ":sparkles: Added" ":fire: Removed" ":zap: Optimized" ":art: Riced" ":construction: WIP")
-SCOPE=$(gum input --placeholder "scope")
+SCOPE=$(gum input --placeholder "Scope")
 
-SUMMARY=$(gum input --value "$TYPE $SCOPE: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --value "$TYPE: $SCOPE - " --placeholder "Summary of this change")
 
 # Commit these changes
 gum confirm "Commit changes?" && git commit -m "$TYPE: $SCOPE"
