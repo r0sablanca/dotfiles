a8697e828e0ec2ccb0f25db28261182dc56e3c66
RosaBlanca <rosablanca@mailfence.com>
2023-07-14 16:51:56 -0700

    :art: Riced:git-commit-helper.sh

--- scripts/git-commit-helper.sh
+++ scripts/git-commit-helper.sh
@@ -3,11 +3,10 @@
 # Created by RosaBlanca
 #
 # July 14, 2023
-
-TYPE=$(gum choose ":sparkles: Added" ":zap: Optimized" ":fire: Removed" ":construction: WIP" ":art: Riced")
+TYPE=$(gum choose ":rocket: Initial Commit" ":sparkles: Added" ":fire: Removed" ":zap: Optimized" ":art: Riced" ":construction: WIP")
 SCOPE=$(gum input --placeholder "scope")
 
-SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --value "$TYPE $SCOPE: " --placeholder "Summary of this change")
 
 # Commit these changes
-gum confirm "Commit changes?" && git commit -m "$TYPE:$SCOPE"
+gum confirm "Commit changes?" && git commit -m "$TYPE: $SCOPE"
